c5ae0237c26f357e4833cb3312e6c437cb7fc479
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-03-22 12:43:38 +0200

    Add routing and a dashboard component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -6,7 +9,29 @@ import { Hero } from '../hero';
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input()
   hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id !== null) {
+      this.heroService.getHero(+id)
+        .subscribe(hero => this.hero = hero);
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
